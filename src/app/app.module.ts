e17b230a0afd1108d68c8e0a19b552309b401a6f
kich <karina03ce@gmail.com>
2024-08-12 14:32:42 -0600

    scripts update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,7 @@ import { DbzModule } from './dbz/dbz.module';
     AppComponent,
   ],
   imports: [
-    BrowserModule,
+
     AppRoutingModule,
     CounterModule,
     HeroesModule,
